5fd05d0cb6bf48218a66df33447b37fe6b28f883
mostafa <mostapha.a.aly@gmail.com>
2019-04-10 12:23:17 +0200

    test

--- config/routes.rb
+++ config/routes.rb
@@ -2,23 +2,20 @@ Rails.application.routes.draw do
   # devise_for :users
   namespace :v1, defaults: {format: :json} do
     #########################
-    post "/user" => "user#create"
-    get "/user" =>"user#index"
-   get "/user/:user_id/friend" => "friend#get_friend"
-   post "/user/:user_id/friend" => "friend#create_friend"
-   delete "/user/:user_id/friend/:friend_id" => "friend#delete_friend"
-   ###############################
-   get "/user/:user_id/order" => "order#get_orders"
-   post "/user/:user_id/order" => "order#create_order"
-   put  "/user/:user_id/order/:order_id" => "order#update_order"
-   put  "/user/:user_id/order/:order_id/status" => "order#update_status"
-   get "/user/:user_id/order/:order_id" => "order#get_order_by_id"
-   put  "/user/:user_id/order/:order_id/friend/:friend_id" => "order#add_friend_to_order"
-   put  "/user/:user_id/order/:order_id/group/:group_id" => "order#add_group_to_order"
-   delete  "/user/:user_id/order/:order_id/group/:group_id" => "order#delete_group_from_order"
+  get "/user/:user_id/friend" => "friend#get_friend"
+  post "/user/:user_id/friend" => "friend#create_friend"
+  delete "/user/:user_id/friend/:friend_id" => "friend#delete_friend"
+  ###############################
+  get "/user/:user_id/order" => "order#get_orders"
+  post "/user/:user_id/order" => "order#create_order"
+  put  "/user/:user_id/order/:order_id" => "order#update_order"
+  put  "/user/:user_id/order/:order_id/status" => "order#update_status"
+  post "/user" => "user#create"
+  get "/user" => "user#index"
 
 
-   ####################
+
+  ####################
     resources :user do
       resources :friend
 
@@ -27,5 +24,5 @@ Rails.application.routes.draw do
     resources :session, only: [:create, :destroy]
     
   end
- 
+
 end
